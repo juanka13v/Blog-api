3aa40625b61c7c916cd0d1244563319cc448f920
Juanka <valerojuanka@gmail.com>
2021-12-24 13:10:38 -0300

    users

--- app.js
+++ app.js
@@ -2,19 +2,21 @@ const dotenv = require("dotenv").config();
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
+const authRoute = require("./routes/auth");
+const userRoute = require("./routes/users");
+
+app.use(express.json());
 
 const connectDB = () => {
-    try {
-        mongoose.connect(process.env.MONGO_URL)
-        console.log("base conectada...");
-    } catch (error) {
-        console.log(error);
-    }
-}
+  try {
+    mongoose.connect(process.env.MONGO_URL);
+    console.log("base conectada...");
+  } catch (error) {
+    console.log(error);
+  }
+};
 
-app.use("/lama", (req, res) => {
-  console.log("hey this is lama url");
-  res.send("hola2");
-});
+app.use("/api/auth", authRoute);
+app.use("/api/users", userRoute)
 
 app.listen("5000", connectDB);
